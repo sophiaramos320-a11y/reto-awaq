171cdb87b0d00050fa596a8a3fa98e07b950fe40
damarispro <damarisp.castrellon@gmail.com>
2026-05-21 23:01:43 -0600

    Nuevas escenas

---
+++ RETO_V1/Assets/Damaris/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 5f;
+
+    void LateUpdate()
+    {
+        Vector3 newPosition = new Vector3(
+            target.position.x,
+            target.position.y,
+            -10f
+        );
+
+        transform.position = Vector3.Lerp(
+            transform.position,
+            newPosition,
+            smoothSpeed * Time.deltaTime
+        );
+    }
+}
